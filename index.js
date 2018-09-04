07d4c550689a4ad29e3d6099a7ca4215d270045d
Hedi Sabah <headesabah@gmail.com>
2018-09-04 20:19:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 // Code your solutions in this file
-const gifts = ['teddy bear', 'drone', 'doll'];
 
-function wrapGifts (gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+function printBadges(names){
+  for(let i=0;i<names.length;i++){
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
   }
- 
-  return gifts;
+  return names
 }
- 
-wrapGifts(gifts);
+
+function tailsNeverFails (tails) {
+  tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
+}
